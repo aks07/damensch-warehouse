d20258441ccd8107ac66fd304472b5884e7ef465
Aditya Singh <aditya.singh@damensch.com>
2022-03-16 22:00:14 +0530

    download csv feature added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { Offline, Online } from "react-detect-offline";
+import { useState, useEffect, useRef } from 'react';
+import { Offline, Online } from 'react-detect-offline';
+import { CSVLink } from 'react-csv';
 import './App.css';
 
 const initForm = {
@@ -8,28 +9,85 @@ const initForm = {
 	scanItem: '',
 };
 
+const dummyCsvFields = {
+	vendor_code: 'VCDAM22',
+	purchase_order_code: 'DA-20-21/135..',
+	qty: '1',
+	cartoncode: 'C001519-1',
+};
+
+const csvHeaders = [
+	{ label: 'Sku Code', key: 'sku_code' },
+	{ label: 'Sku Size', key: 'size' },
+	{ label: 'Color', key: 'color' },
+	{ label: 'Item Code', key: 'barcode' },
+	{ label: 'Vendor Code', key: 'vendor_code' },
+	{ label: 'Purchase Order Code', key: 'purchase_order_code' },
+	{ label: 'Oty', key: 'qty' },
+	{ label: 'cartoncode', key: 'cartoncode' },
+];
+
+const CSVDownload = (props) => {
+  const btnRef = useRef(null)
+  useEffect(() => btnRef.current?.click(), [btnRef])
+  return (
+    <CSVLink {...props}>
+      <span ref={btnRef} />
+    </CSVLink>
+  )
+}
+
 function App() {
 	const [tableNo, setTableNo] = useState(localStorage.getItem('tableNo') || '');
 	const [formData, setFormData] = useState(initForm);
+	const [errorMsg, setErrorMsg] = useState('');
+	const [download, setDownload] = useState({});
 
 	const handleTableNumberSet = (e) => {
 		localStorage.setItem('tableNo', e.target.value);
 		setTableNo(e.target.value);
 	};
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log({ tableNo, ...formData });
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ tableNo, ...formData })
-    })
+	const handleSubmit = async (e) => {
+		try {
+			e.preventDefault();
+			setDownload({});
+
+			const data = {
+				table: tableNo,
+				bin: formData?.binCode,
+				sku: formData?.skuCode,
+				barcode: formData?.scanItem,
+			};
+
+			const response = await fetch(
+				'http://13.235.24.186:3000/process/dummy-success',
+				{
+					// const response = await fetch('http://13.235.24.186:3000/process/dummy-fail', {
+					// const response = await fetch('http://13.235.24.186:3000/process/recode', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(data),
+				}
+			);
+
+			const jsonData = await response.json();
+			console.log('JSON', jsonData);
+
+			if (jsonData.status === 'error') throw jsonData;
+
+			setDownload({ show: true, data: [{ ...jsonData, ...dummyCsvFields }] });
+		} catch (err) {
+			console.log('FAIL!!', err);
+			setErrorMsg(err?.msg || 'Something went wrong!!');
+		}
 	};
 
 	const handleChange = (key, val) => {
+		setDownload({});
+		setErrorMsg('');
 		setFormData((state) => {
 			return { ...state, [key]: val };
 		});
@@ -80,12 +138,34 @@ function App() {
 					</label>
 
 					<input type='submit' value='Enter' />
+					{errorMsg && <div className='error'>{errorMsg}</div>}
 				</form>
 				<div className='item-data-container'>
-					<div>Scanned Items: 05</div>
-					<div>Printed Items: 04</div>
-					<div>Error Items: 01</div>
+					<div>Scanned Items: 0</div>
+					<div>Printed Items: 0</div>
+					<div>Error Items: 0</div>
 				</div>
+				{download?.show && (
+					<>
+						<span className='success'>Success!! </span>
+						{/* <CSVLink
+              filename={`barcode_label_damensch_${new Date().getTime()}.csv`}
+							data={download?.data}
+							headers={csvHeaders}
+							// onClick={() => {
+							// 	setDownload(false);
+							// }}
+						>
+							Download CSV
+						</CSVLink> */}
+						<CSVDownload
+							data={download?.data}
+							headers={csvHeaders}
+							filename={`barcode_label_damensch_${new Date().getTime()}.csv`}
+							target='_blank'
+						/>
+					</>
+				)}
 			</Online>
 			<Offline>
 				<span className='offline'>
